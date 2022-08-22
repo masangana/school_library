2a70f145c7cc6fd78cccab9395cae86357bc1215
masangana <alexmasangana@gmail.com>
2022-08-22 23:18:12 +0300

    capitalize and trimmer

---
+++ trimmer_decorator.rb
@@ -0,0 +1,6 @@
+require_relative './decorator'
+
+class CapitalizeDecorator < Decorator
+    def correct_name
+        @nameable.correct_name.strip.slice(0, 10) if @nameable.correct_name.length > 10    end
+end
