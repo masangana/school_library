5c7e56a1f1028c52041e30b9cc8882fac3126d56
masangana <alexmasangana@gmail.com>
2022-08-24 17:01:46 +0300

    Create relation classroom_student

--- classroom.rb
+++ classroom.rb
@@ -3,5 +3,12 @@ class classroom
 
     def initialize(lable)
         @lable = lable
+        @students = []
     end
+
+    def add_student(student)
+        @student << student
+        student.classroom = self
+    end
+
 end
