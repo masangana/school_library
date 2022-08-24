8786edd889d9e98174a0dff3402d69131a878d8a
masangana <alexmasangana@gmail.com>
2022-08-24 18:43:09 +0300

    Create the many_many rental book

--- .github/workflows/rental.rb
+++ .github/workflows/rental.rb
@@ -1,6 +1,12 @@
 class Rental
     attr_accessor :date
-    def initialize(date)
+    attr_reader :book, :person
+    def initialize(date, person, book)
         @date = date
+        @person = person
+        person.rentals << self
+        @book = book
+        book.rentals << self
     end
+
 end
